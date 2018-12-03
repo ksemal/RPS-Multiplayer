a655c02c4a0d444e2102fb24e3fd5a164ab2a82b
km <42688915+ksemal@users.noreply.github.com>
2018-12-02 23:09:05 -0500

    chat

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,11 +1,11 @@
 var winsAnsw = ["RockScissors", "PaperRock", "ScissorsPaper"];
 var config = {
-  apiKey: "AIzaSyBj9VmNQmbbEbycdfQw4prUtD_IGjqwO-I",
-  authDomain: "myfirstproject-9fc98.firebaseapp.com",
-  databaseURL: "https://myfirstproject-9fc98.firebaseio.com",
-  projectId: "myfirstproject-9fc98",
-  storageBucket: "myfirstproject-9fc98.appspot.com",
-  messagingSenderId: "914471269159"
+  apiKey: "AIzaSyD6SjpeTuRlzjE9wlnNB7xeMKK4gHDZvb8",
+  authDomain: "game-e1f77.firebaseapp.com",
+  databaseURL: "https://game-e1f77.firebaseio.com",
+  projectId: "game-e1f77",
+  storageBucket: "game-e1f77.appspot.com",
+  messagingSenderId: "670933440381"
 };
 firebase.initializeApp(config);
 
@@ -110,6 +110,12 @@ function pressedButton() {
 function showTurn() {
   if (turn === 1) {
     $("#result").text("");
+    $("#name1")
+      .find(".your_choice")
+      .text("");
+    $("#name2")
+      .find(".your_choice")
+      .text("");
     $("#name1").css("border", "1px solid black");
     $("#name2").css("border", "0px solid black");
     if (playerId === "1") {
@@ -134,6 +140,7 @@ function showTurn() {
       $(".turn").text("It's your turn");
     }
   } else if (turn === 3) {
+    $(".choices2").css("visibility", "hidden");
     database.ref("players").once("value", function(snapshot) {
       var firstAnsw = snapshot.child("1").val().choice;
       var secondAnsw = snapshot.child("2").val().choice;
@@ -169,7 +176,7 @@ function showTurn() {
       }
     });
 
-    if (playerId === "1") {
+    if (playerId === "2") {
       var wins = wins1;
       var losses = losses1;
     } else {
@@ -186,112 +193,38 @@ function showTurn() {
         database.ref("players/").update({
           turn: 1
         });
-      }, 5000);
+      }, 8000);
     }
   }
 }
 
-//   database.ref("players/1").update({
-//     choice: $(this).text(),
-//     losses: losses1,
-//     wins: wins1
-//   });
+window.onbeforeunload = function() {
+  console.log("you are leaving");
+};
 
-// database.ref("players/").on("value", function(snapshot) {
-//   if (snapshot.exists()) {
-//     var obj = snapshot.val();
-//     var arr = Object.keys(snapshot.val());
+$("#newMessage").on("click", addNewMessage);
 
-//     for (let i in arr) {
-//       // console.log(arr[i]);
-//       // console.log(obj[arr[i]].name);
-//       $("#name" + arr[i])
-//         .find("h3")
-//         .text(obj[arr[i]].name);
+function addNewMessage(event) {
+  event.preventDefault();
 
-//       $("#name" + arr[i])
-//         .find(".W_L")
-//         .text("Losses: " + obj[arr[i]].losses + " Wins: " + obj[arr[i]].wins);
-//     }
-//   }
-// });
-
-// $(".choices1").on("click", "button", function() {
-//   database.ref("players/1").update({
-//     choice: $(this).text(),
-//     losses: losses1,
-//     wins: wins1
-//   });
-//   $(".choices1").css("visibility", "hidden");
-//   $(".choices2").css("visibility", "hidden");
-//   $("#name1").css("border", "0px solid black");
-//   $("#name2").css("border", "1px solid black");
-//   $(".turn").text("Waiting for your  opponent to choose");
-//   database.ref("players/1").on("value", function(snapshot) {
-//     firstAnsw = snapshot.val().choice;
-//     $("#name1")
-//       .find(".your_choice")
-//       .text(snapshot.val().choice);
-//   });
-// });
-
-// database.ref("players/2").on("value", function(snapshot) {
-//   if (snapshot.child("choice").exists()) {
-//     $(".choices1").css("visibility", "visible");
-//   }
-// });
-
-// $(".choices2").on("click", "button", function() {
-//   database.ref("players/2").update({
-//     choice: $(this).text(),
-//     losses: losses2,
-//     wins: wins2
-//   });
-//   $(".choices1").css("visibility", "hidden");
-//   $(".choices2").css("visibility", "hidden");
-//   $("#name1").css("border", "1px solid black");
-//   $("#name2").css("border", "0px solid black");
-//   database.ref("players/2").on("value", function(snapshot) {
-//     secondAnsw = snapshot.val().choice;
-//     $("#name2")
-//       .find(".your_choice")
-//       .text(snapshot.val().choice);
-//   });
-
-//   database.ref("players").on("value", function(snapshot) {
-//     console.log(wins1, wins2);
-//     var sum =
-//       snapshot.child("1").val().choice + snapshot.child("2").val().choice;
-//     console.log(firstAnsw);
-//     console.log(secondAnsw);
-
-//     if (firstAnsw === secondAnsw) {
-//       $("#result").text("Tie Game");
-//     } else if (winsAnsw.indexOf(sum) >= 0) {
-//       wins1++;
-//       losses2++;
-//       $("#name1")
-//         .find(".W_L")
-//         .text("Losses: " + losses1 + " Wins: " + wins1);
-//       $("#name2")
-//         .find(".W_L")
-//         .text("Losses: " + losses2 + " Wins: " + wins2);
-//       $("#result").text(snapshot.child("1").val().name + " wins!");
-//     } else {
-//       losses1++;
-//       wins2++;
-//       $("#result").text(snapshot.child("2").val().name + " wins!");
-//       $("#name1")
-//         .find(".W_L")
-//         .text("Losses: " + losses1 + " Wins: " + wins1);
-//       $("#name2")
-//         .find(".W_L")
-//         .text("Losses: " + losses2 + " Wins: " + wins2);
-//     }
-//   });
-// });
-// database.ref("players/1").on("value", function(snapshot) {
-//   if (snapshot.child("choice").exists()) {
-//     $(".choices2").css("visibility", "visible");
-//   }
-// });
+  database.ref("players/chat").push({
+    name: $("#name" + playerId)
+      .find("h3")
+      .text(),
+    message: $(".message").val(),
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
+  $(".message").val("");
+}
+database
+  .ref("players/chat")
+  .orderByChild("dateAdded")
+  .on("child_added", function(snapshot) {
+    $(".chat").append(
+      "<p><span>" +
+        snapshot.val().name +
+        ":  </span><span>" +
+        snapshot.val().message +
+        "</span></p>"
+    );
+  });
